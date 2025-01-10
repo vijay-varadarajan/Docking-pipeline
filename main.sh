b0f8a4b3b43908f3c24403b7335544992c84d1f0
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2025-01-10 09:26:11 +0530

    Update PLIP container version in main script

--- main.sh
+++ main.sh
@@ -4,5 +4,5 @@ singularity exec containers/python-3.12.sif ./unzip.sh
 singularity exec containers/python-3.12.sif python filter_docked.py --filter_type best
 singularity exec containers/openbabel-3.1.0.sif ./sdf_to_pdb.sh
 singularity exec containers/python-3.12.sif python combine_ligand_protein.py
-singularity exec containers/plip.sif ./plip_processing.sh
+singularity exec containers/plip-1.0.sif ./plip_processing.sh
 singularity exec containers/python-3.12.sif python xml_processing.py
